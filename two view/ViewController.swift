5e8b39eb45abeca516500cac9ca2262c859f4779
Marat-FMK <kosjachok8@yandex.ru>
2024-04-06 02:17:16 +0300

    Initial Commit

---
+++ two view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  two view
+//
+//  Created by Marat_FMK on 06.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
